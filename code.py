c664e1cb69898ff4bb25030723dfaf7e6339a651
venkatigi <venkateshlang82481@gmail.com>
2022-02-12 10:32:07 +0530

    gg

---
+++ code.py
@@ -0,0 +1,13 @@
+### Name    : Venkatesh E
+### Ref. No : 21003352
+import pandas as pd
+from sklearn import linear_model
+df=pd.read_csv("cars.xls")
+X=df[['Weight','Volume']]
+Y=df['CO2']
+regr=linear_model.LinearRegression()
+regr.fit(X,Y)
+print('Coefficients:',regr.coef_)
+print("Intercept:",regr.intercept_)
+predictedCO2=regr.predict([[1995,522]])
+print("Predicted CO2 emission based on weight and volume:",predictedCO2)
